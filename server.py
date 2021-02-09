29dc4aefe0748ea884d3d0ea7a756ea1cd723392
jngdesign <jng.cad@gmail.com>
2021-02-09 18:43:37 -0500

    serve a listing page using json

--- server.py
+++ server.py
@@ -1,10 +1,47 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_root_dir():
     return 'hello world'
+
+@app.route('/page')
+def page():
+    
+    listing_info='''
+    {
+  "MlsNumber": "X5095381",
+  "PublicRemarks": "Location Location Location !!! Bright Well Maintained One Of Detached Home Back To Trail !!! Newly Wood Floor Throughout, Lots Of Windows, New Paint, Quiet Neighbourhood, Mins To Local Restaurants, Mall, Supermarkets, Schools &Shops. Walk Distense To University Of Waterloo, Used To Be Licensed Student Rental.And Own Use. Buy To Invest Or Own Use. Recent Improvements!!! Can't Miss This.**** EXTRAS **** Stove, Dishwasher, Washer, Dryer, All Elf's, All Window Coverings And Cac. (27853962)",
+  "Bathrooms": 3,
+  "Bedrooms": "4 + 1",
+  "InteriorSize": "",
+  "Type": "House",
+  "Ammenities": "",
+  "Address": "74 KAREN WALK|Waterloo, Ontario N2L6K7",
+  "Longitude": -80.5463426,
+  "Latitude": 43.4591645,
+  "PostalCode": "N2L6K7",
+  "NeighbourHood": "",
+  "Price": "$550,000",
+  "PropertyType": "Single Family",
+  "ParkingSpace": 3,
+  "OwnershipType": "Freehold",
+  "Appliances": "",
+  "FlooringType": "",
+  "BasementType": "N/A (Finished)",
+  "HeatingType": "Forced air (Natural gas)",
+  "LandSize": "39.37 x 124.12 FT",
+  "AmmenitiesNearBy": "",
+  "PropertyTax": "$3,547.12",
+  "ZoningDescription": "",
+  "LowResPhoto": ""
+}
+
+    '''
+    return render_template("public/listing.html", data=json.loads(listing_info))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
